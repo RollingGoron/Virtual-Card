ac1d9a2a891ac5f2a5cd2d5192ec098dfc3d87f7
mredd39 <Manasa.Reddy@aexp.com>
2016-04-11 14:11:22 -0700

    ui chnages

--- Virtual Card/CreateNewCardController.swift
+++ Virtual Card/CreateNewCardController.swift
@@ -34,6 +34,8 @@ class CreateNewCardController: UIViewController, TaskCompletedProtocol {
     
     if firstNameTextField.text == "" || lastNameTextField.text == "" || companyTextField.text == "" || jobTitleTextField.text == "" {
       let alertController = UIAlertController(title: "Sorry!", message: "Please fill out all fields", preferredStyle: .Alert)
+      let defaultAction = UIAlertAction(title: "Okay", style: .Default, handler: nil)
+      alertController.addAction(defaultAction)
       self.presentViewController(alertController, animated: true, completion: nil)
       
     } else {
