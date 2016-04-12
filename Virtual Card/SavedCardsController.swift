56271c5336b270ecf9b3ddaa3207959fd117653a
mredd39 <Manasa.Reddy@aexp.com>
2016-04-12 08:42:52 -0700

    ui changes.

--- Virtual Card/SavedCardsController.swift
+++ Virtual Card/SavedCardsController.swift
@@ -9,37 +9,66 @@
 import UIKit
 
 class SavedCardsController: UIViewController {
-  @IBOutlet weak var savedCardsTableView: UITableView!
-  var tableData = NSArray()
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    self.title = "Title"
-    tableData = CoreDataManager.returnAllSavedCards()
-    savedCardsTableView.reloadData()
-    self.savedCardsTableView.rowHeight = 101
+    @IBOutlet weak var savedCardsTableView: UITableView!
+    var tableData = NSArray()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Title"
+        
+        tableData = CoreDataManager.returnAllSavedCards()
+        
+        savedCardsTableView.reloadData()
+        self.savedCardsTableView.registerNib(UINib(nibName: "SavedCell", bundle: nil), forCellReuseIdentifier: "SavedCell")
+        self.savedCardsTableView.rowHeight = 87
+        self.savedCardsTableView.estimatedRowHeight = UITableViewAutomaticDimension
+        
+        NetworkManager.sharedInstance.fetchAllCardsForAccount { (returnedObject, returnedString, returnedBool) -> Void in
+            
+            guard let returnedCardsArray = returnedObject["busiunessCards"] as? [Dictionary<String, String>] else {
+                return
+            }
+            
+            if returnedCardsArray.count != self.tableData.count { // Check if new cards have been added.
+                
+                for (var i = 0; i < returnedCardsArray.count; i++) {
+                    print(returnedCardsArray[i])
+                    
+                    CoreDataManager.saveReceivedCardToCoreData(CardModel(fetchedDictionary: returnedCardsArray[i]))
+                }
+            }
+            
+        }
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
     
     // Do any additional setup after loading the view, typically from a nib.
   }
   
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
-  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return tableData.count
-  }
-  
-  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//  override func didReceiveMemoryWarning() {
+//    super.didReceiveMemoryWarning()
+//    // Dispose of any resources that can be recreated.
+//  }
+//  
+extension SavedCardsController : UITableViewDataSource, UITableViewDelegate {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
     
-    let cell = tableView.dequeueReusableCellWithIdentifier("CardCell") as! CardCell
-    let myCard = tableData[indexPath.row] as! CardEntity
-    let aStr = String(format: "%@ %@", myCard.firstName!,myCard.lastName!)
-    cell.nameLabel.text = aStr
-    cell.jobLabel.text = myCard.jobTitle
-    cell.companyLabel.text = myCard.company
-    
-    return cell
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let savedEntity = tableData[indexPath.row] as! SavedEntity
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("SavedCell") as! SavedCell
+        
+        cell.firstNameLabel.text = savedEntity.firstName
+        cell.lastNameLabel.text = savedEntity.lastName
+        cell.jobTitleLabel.text = savedEntity.jobTitle
+        cell.companyTitle.text = savedEntity.company
+        
+        return cell
+        
+    }
     
   }
   
@@ -47,7 +76,7 @@ class SavedCardsController: UIViewController {
     
   }
   
-}
+
 
 class CardCell: UITableViewCell {
   
