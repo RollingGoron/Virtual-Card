11474135fc1b6c1caa4180904027e96e9821de11
pgosling <Peter.Gosling@aexp.com>
2016-04-12 13:32:41 -0700

    Added check if business card exists

--- Virtual Card/CoreDataManager.swift
+++ Virtual Card/CoreDataManager.swift
@@ -10,115 +10,119 @@ import UIKit
 import CoreData
 
 class CoreDataManager: NSObject {
+  
+  class func saveReceivedCardToCoreData(cardModel : CardModel) {
     
-    class func saveReceivedCardToCoreData(cardModel : CardModel) {
-        
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        let managedObjectContext = appDelegate.managedObjectContext
-        let entity =  NSEntityDescription.entityForName("SavedEntity", inManagedObjectContext: managedObjectContext)
-        let cardEntity = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedObjectContext) as! SavedEntity
-        
-        
-        cardEntity.firstName = cardModel.cardFirstName
-        cardEntity.lastName = cardModel.cardLastName
-        cardEntity.company = cardModel.cardCompany
-        cardEntity.jobTitle = cardModel.cardJobTitle
-        cardEntity.email = cardModel.cardEmail
-        cardEntity.address = cardModel.cardAddress
-        cardEntity.userID = cardModel.cardUserID
-        cardEntity.phoneNumber = cardModel.cardPhoneNumber
-        
-        do {
-            try managedObjectContext.save()
-            print("Saved card info!")
-        } catch let error as NSError {
-            print("An error occured when saving \(error)")
-        }
-        
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    let managedObjectContext = appDelegate.managedObjectContext
+    let entity =  NSEntityDescription.entityForName("SavedEntity", inManagedObjectContext: managedObjectContext)
+    let cardEntity = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedObjectContext) as! SavedEntity
+    
+    
+    cardEntity.firstName = cardModel.cardFirstName
+    cardEntity.lastName = cardModel.cardLastName
+    cardEntity.company = cardModel.cardCompany
+    cardEntity.jobTitle = cardModel.cardJobTitle
+    cardEntity.email = cardModel.cardEmail
+    cardEntity.address = cardModel.cardAddress
+    cardEntity.userID = cardModel.cardUserID
+    cardEntity.phoneNumber = cardModel.cardPhoneNumber
+    
+    do {
+      try managedObjectContext.save()
+      print("Saved card info!")
+    } catch let error as NSError {
+      print("An error occured when saving \(error)")
     }
     
-    class func saveMyCardToCoreData(cardModel : CardModel) -> Bool? {
-        
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        let managedObjectContext = appDelegate.managedObjectContext
-        let entity =  NSEntityDescription.entityForName("CardEntity", inManagedObjectContext: managedObjectContext)
-        let cardEntity = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedObjectContext) as! CardEntity
-        
-        
-        cardEntity.firstName = cardModel.cardFirstName
-        cardEntity.lastName = cardModel.cardLastName
-        cardEntity.company = cardModel.cardCompany
-        cardEntity.jobTitle = cardModel.cardJobTitle
-        cardEntity.email = cardModel.cardEmail
-        cardEntity.address = cardModel.cardAddress
-        cardEntity.userID = cardModel.cardUserID
-        cardEntity.phoneNumber = cardModel.cardPhoneNumber
-        
-        do {
-            try managedObjectContext.save()
-            print("Saved card info!")
-            return true
-        } catch let error as NSError {
-            print("An error occured when saving \(error)")
-        }
-        return false
+  }
+  
+  class func saveMyCardToCoreData(cardModel : CardModel) -> Bool? {
+    
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    let managedObjectContext = appDelegate.managedObjectContext
+    let entity =  NSEntityDescription.entityForName("CardEntity", inManagedObjectContext: managedObjectContext)
+    let cardEntity = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedObjectContext) as! CardEntity
+    
+    
+    cardEntity.firstName = cardModel.cardFirstName
+    cardEntity.lastName = cardModel.cardLastName
+    cardEntity.company = cardModel.cardCompany
+    cardEntity.jobTitle = cardModel.cardJobTitle
+    cardEntity.email = cardModel.cardEmail
+    cardEntity.address = cardModel.cardAddress
+    cardEntity.userID = cardModel.cardUserID
+    cardEntity.phoneNumber = cardModel.cardPhoneNumber
+    
+    do {
+      try managedObjectContext.save()
+      print("Saved card info!")
+      return true
+    } catch let error as NSError {
+      print("An error occured when saving \(error)")
+    }
+    return false
+  }
+  
+  class func returnMyBusinessCard() -> CardModel? {
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    let managedObjectContext = appDelegate.managedObjectContext
+    let fetchRequest = NSFetchRequest(entityName: "CardEntity")
+    
+    do {
+      let fetchResults = try managedObjectContext.executeFetchRequest(fetchRequest)
+      
+      if fetchResults.count == 0 {
+        return nil
+      }
+      
+      let savedCard = fetchResults as! [CardEntity]
+      
+      let myCard = savedCard[0]
+      let cardModel = CardModel(cardEntity: myCard)
+      print("Returned Card Data")
+      return cardModel
+      
+    } catch {
+      print("Error fetching saved card!")
+      return nil
     }
     
-    class func returnMyBusinessCard() -> CardModel? {
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        let managedObjectContext = appDelegate.managedObjectContext
-        let fetchRequest = NSFetchRequest(entityName: "CardEntity")
-        
-        do {
-            let fetchResults = try managedObjectContext.executeFetchRequest(fetchRequest)
-            let savedCard = fetchResults as! [CardEntity]
-            let myCard = savedCard[0]
-            let cardModel = CardModel(cardEntity: myCard)
-            print("Returned Card Data")
-            return cardModel
-            
-        } catch {
-            print("Error fetching saved card!")
-            return nil
-        }
-        
+  }
+  
+  class func returnAllSavedCards()-> NSMutableArray {
+    
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    let managedObjectContext = appDelegate.managedObjectContext
+    let fetchRequest = NSFetchRequest(entityName: "SavedEntity")
+    
+    let returnedArray : NSMutableArray = []
+    
+    do {
+      let fetchResults = try managedObjectContext.executeFetchRequest(fetchRequest)
+      let savedCards = fetchResults as NSArray
+      returnedArray.addObjectsFromArray(savedCards as! [SavedEntity])
+      return returnedArray
+      
+    } catch {
+      print("Error fetching saved card!")
+      return NSMutableArray()
+    }
+  }
+  
+  class func removeSavedItemFromCoreData(savedEntity : SavedEntity) {
+    
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    let managedObjectContext = appDelegate.managedObjectContext
+    managedObjectContext.deleteObject(savedEntity)
+    do {
+      try managedObjectContext.save()
+    } catch let error as NSError {
+      "An error occured when deleting: \(error)"
     }
     
-    class func returnAllSavedCards()-> NSMutableArray {
-        
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        let managedObjectContext = appDelegate.managedObjectContext
-        let fetchRequest = NSFetchRequest(entityName: "SavedEntity")
-        
-        let returnedArray : NSMutableArray = []
-        
-        do {
-            let fetchResults = try managedObjectContext.executeFetchRequest(fetchRequest)
-            let savedCards = fetchResults as NSArray
-            
-            returnedArray.addObjectsFromArray(savedCards as! [SavedEntity])
-            
-            return returnedArray
-            
-        } catch {
-            print("Error fetching saved card!")
-            return NSMutableArray()
-        }
-    }
     
-    class func removeSavedItemFromCoreData(savedEntity : SavedEntity) {
-        
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        let managedObjectContext = appDelegate.managedObjectContext
-        managedObjectContext.deleteObject(savedEntity)
-        do {
-            try managedObjectContext.save()
-        } catch let error as NSError {
-            "An error occured when deleting: \(error)"
-        }
-       
-        
-        
-    }
-
+    
+  }
+  
 }
